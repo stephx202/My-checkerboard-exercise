88e0444a77c7f8c41872b9c86444d5a25803dfd9
stephx202 <stephhernandez202@gmail.com>
2023-11-16 21:47:43 -0800

    randomColors

--- script.js
+++ script.js
@@ -7,31 +7,55 @@ box.style.height = "800px";
 box.style.background = "red"
 document.body.appendChild(box);
 
-function littleblackbox(){
-    for(let i=0; i<8; i++){
-         
-        for(let j = 0; j< 8; j++){
-            //console.log(`i = ${i}, j = ${j}`)
-            let column  = document.createElement("div")
-           // column.textContent= i;
-            column.style.outline = "solid white 1px"
-            column.style.width= "12.5%";
-            column.style.height = "12.5%";
-            column.style.display = "flex"
-            column.style.flexwrap = "wrap"
-            box.appendChild(column);
-            if((i %2 ===0) && (j %2===0)){
-             column.style.background = "black";
-            }else if((i % 2===1) && (j %2===1)){
-                column.style.background = "black";
-            }
 
-        }
+function randomColorBox(){
+    for (let i = 0; i < 64; i++) {
+   
+    let littleColorBox = document.createElement("div");
+    let redColorValue = Math.floor(Math.random() * 255);
+    let greenColorValue = Math.floor(Math.random() * 255);
+    let blueColorValue = Math.floor(Math.random() * 255);
 
-  
+
+    littleColorBox.style.outline = "solid white 1px"
+    littleColorBox.style.width= "12.5%";
+    littleColorBox.style.height = "12.5%";
+    littleColorBox.style.display = "flex"
+    littleColorBox.style.flexwrap = "wrap";
+    littleColorBox.style.backgroundColor = `rgb(${redColorValue}, ${greenColorValue}, ${blueColorValue})`
+    box.appendChild(littleColorBox)
     }
-
-
-    
 }
-littleblackbox()
+console.log(randomColorBox())
+
+
+// function littleblackbox(){
+//     for(let i=0; i < 8; i++){
+
+//         for(let j = 0; j< 8; j++){
+//             //console.log(`i = ${i}, j = ${j}`)
+//             let littleBox  = document.createElement("div")
+//            // column.textContent= i;
+//             // littleBox.style.outline = "solid white 1px"
+//             // littleBox.style.width= "12.5%";
+//             // littleBox.style.height = "12.5%";
+//             // littleBox.style.display = "flex"
+//             // littleBox.style.flexwrap = "wrap";
+//             // littleBox.style.backgroundColor = `rgb(${redColorValue}, ${greenColorValue}, ${blueColorValue})`
+//             // console.log(littleBox)
+//             // box.appendChild(littleBox);
+//             // if((i %2 ===0) && (j %2===0)){
+//             //  column.style.background = "black";
+//             // }else if((i % 2===1) && (j %2===1)){
+//             //     column.style.background = `rgb(${redColorValue}, ${greenColorValue}, ${blueColorValue})`;
+//             // }
+//         }
+        
+//     }
+// }
+// document.addEventListener("click", function() {
+//     alert("You clicked on the page!");
+// });
+  
+// littleblackbox()
+
