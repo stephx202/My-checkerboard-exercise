b9d6c2a98fbaafce1586b6fa2b74765295278e40
stephx202 <stephhernandez202@gmail.com>
2023-11-16 22:00:06 -0800

    RandomColors

--- script.js
+++ script.js
@@ -29,33 +29,3 @@ function randomColorBox(){
 console.log(randomColorBox())
 
 
-// function littleblackbox(){
-//     for(let i=0; i < 8; i++){
-
-//         for(let j = 0; j< 8; j++){
-//             //console.log(`i = ${i}, j = ${j}`)
-//             let littleBox  = document.createElement("div")
-//            // column.textContent= i;
-//             // littleBox.style.outline = "solid white 1px"
-//             // littleBox.style.width= "12.5%";
-//             // littleBox.style.height = "12.5%";
-//             // littleBox.style.display = "flex"
-//             // littleBox.style.flexwrap = "wrap";
-//             // littleBox.style.backgroundColor = `rgb(${redColorValue}, ${greenColorValue}, ${blueColorValue})`
-//             // console.log(littleBox)
-//             // box.appendChild(littleBox);
-//             // if((i %2 ===0) && (j %2===0)){
-//             //  column.style.background = "black";
-//             // }else if((i % 2===1) && (j %2===1)){
-//             //     column.style.background = `rgb(${redColorValue}, ${greenColorValue}, ${blueColorValue})`;
-//             // }
-//         }
-        
-//     }
-// }
-// document.addEventListener("click", function() {
-//     alert("You clicked on the page!");
-// });
-  
-// littleblackbox()
-
